889f7b1674e7088ddccaf06482f68aca05c9d7e0
Saurabh Gupta <saurabh.gupta@bayer.com>
2024-05-12 14:17:23 -0400

    changes to build in Android and Web

--- Source/iot-device-app-docker/workspace/src/app/home/home.page.ts
+++ Source/iot-device-app-docker/workspace/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,16 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  appTitle: string = 'blank';
+  constructor() {
+
+    if (Capacitor.getPlatform() === 'ios') {
+      this.appTitle = "I am a iOS app.";
+    } else if (Capacitor.getPlatform() === 'android') {
+      this.appTitle = "I am a Android app.";
+    } else {
+      this.appTitle = "I am a web browser.";
+    }
+  }
+
 }
